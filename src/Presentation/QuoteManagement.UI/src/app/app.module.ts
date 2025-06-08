87a822dd6e6f020c22ae683f9daaa3c4d8fa4eca
AenesC <aenesc1@gmail.com>
2025-06-08 15:44:23 +0300

    feat(ui): add primeng-based layout

--- src/Presentation/QuoteManagement.UI/src/app/app.module.ts
+++ src/Presentation/QuoteManagement.UI/src/app/app.module.ts
@@ -1,34 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesListComponent } from './features/quotes/quotes-list/quotes-list.component';
 import { QuoteFormComponent } from './features/quotes/quote-form/quote-form.component';
-import { AuthorsListComponent } from './features/authors/authors-list/authors-list.component';
-import { AuthorFormComponent } from './features/authors/author-form/author-form.component';
-import { BooksListComponent } from './features/books/books-list/books-list.component';
-import { BookFormComponent } from './features/books/book-form/book-form.component';
+import { ProfileComponent } from './features/profile/profile.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { TabViewModule } from 'primeng/tabview';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesListComponent,
     QuoteFormComponent,
-    AuthorsListComponent,
-    AuthorFormComponent,
-    BooksListComponent,
-    BookFormComponent,
+    ProfileComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
+    MenubarModule,
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    TabViewModule,
+    SpeedDialModule,
+    ProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
